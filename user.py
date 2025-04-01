02f05f4056804bb34de993656fa671f05ffde8c4
weversonbarbieri <65040693+weversonbarbieri@users.noreply.github.com>
2025-04-01 20:26:33 -0300

    Add files via upload

---
+++ user.py
@@ -0,0 +1,13 @@
+
+# This is the class name
+class User:
+    # Class attributes
+    default_language = "English"
+
+
+    # Constructor method to initialize the object
+    def __init__(self, name, username, language):
+        # Instance attributes
+        self.name = name
+        self.username = username
+        self.language = language
